f00cc8a7e655dec794ee4c70c9804d9ba30eba99
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-05-01 17:44:24 +0200

    trying endpoint change because of nginx issues

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ console.log("Loaded index.js");
 socket.on('roomCreated', function (roomId) {
     console.log('Room created:', roomId);
     console.log(socket);
-    fetch(`/waitroom/${roomId}`)
+    fetch(`rockpaperscissors/waitroom/${roomId}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
