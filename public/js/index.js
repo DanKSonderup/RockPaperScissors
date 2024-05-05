5f62894d529adb4c5f6151e4e6d850ffab7d1ca3
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2024-05-05 14:30:52 +0200

    debugging added

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,17 @@ function constructUrl(path, roomId) {
     return `${baseUrl}${path}/${roomId}`;
 }
 
+socket.on("connect_error", (err) => {
+    // the reason of the error, for example "xhr poll error"
+    console.log(err.message);
+
+    // some additional description, for example the status code of the initial HTTP response
+    console.log(err.description);
+
+    // some additional context, for example the XMLHttpRequest object
+    console.log(err.context);
+});
+
 socket.on('roomCreated', function (roomId) {
     console.log('Room created:', roomId);
     fetch(constructUrl('waitroom', roomId))
